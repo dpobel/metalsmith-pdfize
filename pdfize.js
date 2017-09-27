633c8035991fb939dcc2e798abc87d20381c56d7
Damien Pobel <damien@pobel.fr>
2017-09-27 15:39:29 +0200

    Added .gitignore

--- pdfize.js
+++ pdfize.js
@@ -4,10 +4,11 @@ const multimatch = require('multimatch');
 const async = require('async');
 
 function pdfize(serverInfo, browser, printOptions, files, path, callback) {
-    const pdfPath = path + '.pdf'; // TODO not super nice
+    const pdfPath = path + '.pdf'; // FIXME see https://github.com/dpobel/metalsmith-pdfize/issues/4
+    const internalUrl = `http://${serverInfo.address}:${serverInfo.port}/${path}`;
 
     browser.newPage()
-        .then((page) => page.goto(`http://${serverInfo.address}:${serverInfo.port}/${path}`, {waitUntil: 'networkidle'})
+        .then((page) => page.goto(internalUrl, {waitUntil: 'networkidle'})
             .then(() => page.pdf(printOptions))
             .catch(callback)
         )
