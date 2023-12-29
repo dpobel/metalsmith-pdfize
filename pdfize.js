9af8cc2ee2b753a71320b97ac44da2ae64656152
Damien Pobel <damien.pobel@hiboo.io>
2023-12-29 09:10:29 +0100

    chore: configure prettier

--- pdfize.js
+++ pdfize.js
@@ -1,59 +1,68 @@
-const http = require('http');
-const puppeteer = require('puppeteer');
-const multimatch = require('multimatch');
-const async = require('async');
+const http = require("http");
+const puppeteer = require("puppeteer");
+const multimatch = require("multimatch");
+const async = require("async");
 
 function pdfize(serverInfo, browser, printOptions, files, path, callback) {
-    const pdfPath = path + '.pdf'; // FIXME see https://github.com/dpobel/metalsmith-pdfize/issues/4
-    const internalUrl = `http://${serverInfo.address}:${serverInfo.port}/${path}`;
+  const pdfPath = path + ".pdf"; // FIXME see https://github.com/dpobel/metalsmith-pdfize/issues/4
+  const internalUrl = `http://${serverInfo.address}:${serverInfo.port}/${path}`;
 
-    browser.newPage()
-        .then((page) => page.goto(internalUrl, {waitUntil: 'networkidle0'})
-            .then(() => page.pdf(printOptions))
-            .catch(callback)
-        )
-        .then((buffer) => {
-            files[pdfPath] = {
-                path: pdfPath,
-                contents: buffer,
-            };
-            callback();
-        })
-        .catch(callback);
+  browser
+    .newPage()
+    .then((page) =>
+      page
+        .goto(internalUrl, { waitUntil: "networkidle0" })
+        .then(() => page.pdf(printOptions))
+        .catch(callback),
+    )
+    .then((buffer) => {
+      files[pdfPath] = {
+        path: pdfPath,
+        contents: buffer,
+      };
+      callback();
+    })
+    .catch(callback);
 }
 
 function serveMetalsmithFiles(files) {
-    return function (req, res) {
-        const localPath = req.url.replace(/^\/+/, '');
+  return function (req, res) {
+    const localPath = req.url.replace(/^\/+/, "");
 
-        if ( !files[localPath] ) {
-            console.warn(`[metalsmith-pdfize] Unable to serve "${req.url}" from file list`);
-            res.writeHead(404);
-            return res.end();
-        }
-        res.writeHead(200);
-        res.end(files[localPath].contents);
-    };
+    if (!files[localPath]) {
+      console.warn(
+        `[metalsmith-pdfize] Unable to serve "${req.url}" from file list`,
+      );
+      res.writeHead(404);
+      return res.end();
+    }
+    res.writeHead(200);
+    res.end(files[localPath].contents);
+  };
 }
 
 module.exports = function (options) {
-    return function (files, metalsmith, done) {
-        const server = http.createServer(serveMetalsmithFiles(files));
-        const toExport = multimatch(Object.keys(files), options.pattern);
+  return function (files, metalsmith, done) {
+    const server = http.createServer(serveMetalsmithFiles(files));
+    const toExport = multimatch(Object.keys(files), options.pattern);
 
-        server.listen(0, '127.0.0.1', () => {
-            puppeteer.launch(options.launchOptions).then((browser) => {
-                const pdfizeFn = pdfize.bind(null, server.address(), browser, options.printOptions, files);
+    server.listen(0, "127.0.0.1", () => {
+      puppeteer.launch(options.launchOptions).then((browser) => {
+        const pdfizeFn = pdfize.bind(
+          null,
+          server.address(),
+          browser,
+          options.printOptions,
+          files,
+        );
 
-                async.each(toExport, pdfizeFn, (err) => {
-                    server.close();
-                    browser.close().then(() => {
-                        done(err);
-                    });
-                });
-            });
+        async.each(toExport, pdfizeFn, (err) => {
+          server.close();
+          browser.close().then(() => {
+            done(err);
+          });
         });
-    };
+      });
+    });
+  };
 };
-
-
--- tests/functional.js
+++ tests/functional.js
@@ -1,42 +1,44 @@
-const metalsmith = require('metalsmith');
-const pdfize = require('../pdfize');
-const fs = require('fs');
-const assert = require('assert');
-const isPdf = require('is-pdf');
-const path = require('path');
+const metalsmith = require("metalsmith");
+const pdfize = require("../pdfize");
+const fs = require("fs");
+const assert = require("assert");
+const isPdf = require("is-pdf");
+const path = require("path");
 
-describe('Metalsmith pdfize functional tests', function () {
-    const buildDir = 'build/';
-    const ms = metalsmith(__dirname);
+describe("Metalsmith pdfize functional tests", function () {
+  const buildDir = "build/";
+  const ms = metalsmith(__dirname);
 
-    beforeEach(function (done) {
-        this.timeout(5000);
-        ms.clean(true);
-        ms.use(pdfize({
-            pattern: ['*pdf.html'],
-            printOptions: {
-                printBackground: true,
-                format: 'A4',
-            },
-        }));
-        ms.destination(buildDir);
-        ms.build(function (error) {
-            done(error);
-        });
+  beforeEach(function (done) {
+    this.timeout(5000);
+    ms.clean(true);
+    ms.use(
+      pdfize({
+        pattern: ["*pdf.html"],
+        printOptions: {
+          printBackground: true,
+          format: "A4",
+        },
+      }),
+    );
+    ms.destination(buildDir);
+    ms.build(function (error) {
+      done(error);
     });
+  });
 
-    it('should produce pdfs', function () {
-        const fileDir = path.join(__dirname, buildDir);
+  it("should produce pdfs", function () {
+    const fileDir = path.join(__dirname, buildDir);
 
-        assert(!fs.existsSync(fileDir + 'random.html.pdf'));
-        assert(fs.existsSync(fileDir + 'random.html'));
+    assert(!fs.existsSync(fileDir + "random.html.pdf"));
+    assert(fs.existsSync(fileDir + "random.html"));
 
-        assert(fs.existsSync(fileDir + 'another-to-pdf.html.pdf'));
-        assert(isPdf(fs.readFileSync(fileDir + 'another-to-pdf.html.pdf')));
-        assert(fs.existsSync(fileDir + 'another-to-pdf.html'));
+    assert(fs.existsSync(fileDir + "another-to-pdf.html.pdf"));
+    assert(isPdf(fs.readFileSync(fileDir + "another-to-pdf.html.pdf")));
+    assert(fs.existsSync(fileDir + "another-to-pdf.html"));
 
-        assert(fs.existsSync(fileDir + 'i-want-a-pdf.html.pdf'));
-        assert(isPdf(fs.readFileSync(fileDir + 'i-want-a-pdf.html.pdf')));
-        assert(fs.existsSync(fileDir + 'i-want-a-pdf.html'));
-    });
+    assert(fs.existsSync(fileDir + "i-want-a-pdf.html.pdf"));
+    assert(isPdf(fs.readFileSync(fileDir + "i-want-a-pdf.html.pdf")));
+    assert(fs.existsSync(fileDir + "i-want-a-pdf.html"));
+  });
 });
