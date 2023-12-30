acf40b79bfb0cd8e76141fbde6786a7b1af2c538
Damien Pobel <damien@pobel.fr>
2023-12-30 10:05:59 +0100

    test: remove unit tests and improve functional tests

--- tests/functional.js
+++ tests/functional.js
@@ -4,41 +4,116 @@ const fs = require("fs");
 const assert = require("assert");
 const isPdf = require("is-pdf");
 const path = require("path");
+const sinon = require("sinon");
 
 describe("Metalsmith pdfize functional tests", function () {
   const buildDir = "build/";
-  const ms = metalsmith(__dirname);
+  const fileDir = path.join(__dirname, buildDir);
 
-  beforeEach(function (done) {
-    this.timeout(5000);
+  const build = async ({
+    pattern,
+    printOptions = undefined,
+    launchOptions = undefined,
+  }) => {
+    const ms = metalsmith(__dirname);
     ms.clean(true);
     ms.use(
       pdfize({
-        pattern: ["*pdf.html"],
-        printOptions: {
-          printBackground: true,
-          format: "A4",
-        },
+        pattern,
+        printOptions,
+        launchOptions,
       }),
     );
     ms.destination(buildDir);
-    ms.build(function (error) {
-      done(error);
+    await ms.build();
+  };
+
+  describe("matching", () => {
+    it("should create a pdf for matched documents", async function () {
+      await build({ pattern: "*pdf.html" });
+      assert(!fs.existsSync(fileDir + "random.html.pdf"));
+      assert(fs.existsSync(fileDir + "random.html"));
+
+      assert(fs.existsSync(fileDir + "another-to-pdf.html.pdf"));
+      assert(isPdf(fs.readFileSync(fileDir + "another-to-pdf.html.pdf")));
+      assert(fs.existsSync(fileDir + "another-to-pdf.html"));
+
+      assert(fs.existsSync(fileDir + "i-want-a-pdf.html.pdf"));
+      assert(isPdf(fs.readFileSync(fileDir + "i-want-a-pdf.html.pdf")));
+      assert(fs.existsSync(fileDir + "i-want-a-pdf.html"));
+    });
+
+    it("should support several match pattern", async function () {
+      await build({ pattern: ["i-want-a-pdf*", "another-to-pdf*"] });
+      assert(!fs.existsSync(fileDir + "random.html.pdf"));
+      assert(fs.existsSync(fileDir + "random.html"));
+
+      assert(fs.existsSync(fileDir + "another-to-pdf.html.pdf"));
+      assert(isPdf(fs.readFileSync(fileDir + "another-to-pdf.html.pdf")));
+      assert(fs.existsSync(fileDir + "another-to-pdf.html"));
+
+      assert(fs.existsSync(fileDir + "i-want-a-pdf.html.pdf"));
+      assert(isPdf(fs.readFileSync(fileDir + "i-want-a-pdf.html.pdf")));
+      assert(fs.existsSync(fileDir + "i-want-a-pdf.html"));
     });
   });
 
-  it("should produce pdfs", function () {
-    const fileDir = path.join(__dirname, buildDir);
+  describe("options", function () {
+    let pdfjsLib;
 
-    assert(!fs.existsSync(fileDir + "random.html.pdf"));
-    assert(fs.existsSync(fileDir + "random.html"));
+    beforeEach(async function () {
+      pdfjsLib = await import("pdfjs-dist");
+    });
 
-    assert(fs.existsSync(fileDir + "another-to-pdf.html.pdf"));
-    assert(isPdf(fs.readFileSync(fileDir + "another-to-pdf.html.pdf")));
-    assert(fs.existsSync(fileDir + "another-to-pdf.html"));
+    describe("print options", () => {
+      it("should take print options into account", async function () {
+        await build({
+          pattern: "random.html",
+          printOptions: {
+            height: "500px",
+            width: "500px",
+          },
+        });
+        const pdf = await pdfjsLib.getDocument(fileDir + "random.html.pdf")
+          .promise;
 
-    assert(fs.existsSync(fileDir + "i-want-a-pdf.html.pdf"));
-    assert(isPdf(fs.readFileSync(fileDir + "i-want-a-pdf.html.pdf")));
-    assert(fs.existsSync(fileDir + "i-want-a-pdf.html"));
+        const page = await pdf.getPage(1);
+        const { pageWidth, pageHeight } = page.getViewport().rawDims;
+        assert.equal(pageWidth, pageHeight);
+      });
+    });
+
+    describe("launch options", () => {
+      it("should take launch options into account", async function () {
+        await build({
+          pattern: "random.html",
+          launchOptions: {
+            args: ["--user-agent=metalsmith-pdfize"],
+          },
+        });
+        const pdf = await pdfjsLib.getDocument(fileDir + "random.html.pdf")
+          .promise;
+
+        const metadata = await pdf.getMetadata();
+        assert(metadata.info.Creator, "metalsmith-pdfize");
+      });
+    });
+  });
+
+  describe("internal server", function () {
+    beforeEach(function () {
+      sinon.stub(console, "warn");
+    });
+
+    afterEach(function () {
+      console.warn.restore();
+    });
+
+    it("should warn about broken external reference", async function () {
+      await build({ pattern: "broken.html" });
+      assert(fs.existsSync(fileDir + "broken.html.pdf"));
+      assert(fs.existsSync(fileDir + "broken.html"));
+      assert(console.warn.calledOnce, "A warning should have been generated");
+    });
   });
 });
