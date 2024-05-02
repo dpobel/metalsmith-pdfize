8f15705a8be4a78b1901b8552a14ec365946e9ef
Damien Pobel <damien@pobel.fr>
2024-05-02 18:12:23 +0200

    chore(eslint): switch to new eslint config format

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: {
+        ...globals.node,
+        ...globals.es6,
+      },
+    },
+  },
+  {
+    files: ["tests/**"],
+    languageOptions: {
+      globals: {
+        ...globals.mocha,
+      },
+    },
+  },
+];
